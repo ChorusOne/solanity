5aa3ee7654f8eb5a36050d53d9f59848da5cdac4
Stephen Akridge <sakridge@gmail.com>
2018-09-25 14:23:17 -0700

    AES and chacha source from OpenSSL
    https://github.com/openssl/openssl
    
    @ 345bee916ac5ad28dd269afc24c27fe234c4045b

---
+++ src/cuda-crypt/chacha.h
@@ -0,0 +1,21 @@
+#ifndef HEADER_CHACHA_H
+# define HEADER_CHACHA_H
+
+#include <inttypes.h>
+# include <stddef.h>
+# ifdef  __cplusplus
+extern "C" {
+# endif
+
+#define CHACHA_KEY_SIZE 32
+#define CHACHA_NONCE_SIZE 12
+
+void chacha20_encrypt_ctr(uint8_t *out, const uint8_t *in, size_t in_len,
+                          const uint8_t key[CHACHA_KEY_SIZE], const uint8_t nonce[CHACHA_NONCE_SIZE],
+                          uint32_t counter);
+
+# ifdef  __cplusplus
+}
+# endif
+
+#endif
