c9b3f61eb0c78df88d28a31915128806404fbcb1
Michael Vines <mvines@gmail.com>
2018-12-19 15:18:12 -0800

    Run s3cmd in a container to avoid additional CI system dependencies

--- ci/build.sh
+++ ci/build.sh
@@ -43,13 +43,13 @@ set -x
 
 TOOLCHAIN=x86_64-unknown-linux-gnu # TODO: Remove hard code
 
-if [[ ! -r s3cmd-2.0.1/s3cmd ]]; then
-  rm -rf s3cmd-2.0.1.tar.gz s3cmd-2.0.1
-  wget https://github.com/s3tools/s3cmd/releases/download/v2.0.1/s3cmd-2.0.1.tar.gz
-  tar zxf s3cmd-2.0.1.tar.gz
-fi
-
-python ./s3cmd-2.0.1/s3cmd --acl-public put solana-perf.tgz \
+docker run \
+  --rm \
+  --env AWS_ACCESS_KEY_ID \
+  --env AWS_SECRET_ACCESS_KEY \
+  --volume "$PWD:/solana" \
+  eremite/aws-cli:2018.12.18 \
+  /usr/bin/s3cmd --acl-public put /solana/solana-perf.tgz \
   s3://solana-perf/$BRANCH/$TOOLCHAIN/solana-perf.tgz
 
 exit 0
